d6841c3eb2dc93efdf00a63cd7df87eb2ced5ad4
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-17 17:58:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   least_a = nil 
   least_b = nil 
   name_hash.each do |a,b|
-    if least_a == nil || a < least_a 
-      least_a = a 
+    if least_b == nil || b < least_b 
+      least_b = b
       least_b
     end
   end
