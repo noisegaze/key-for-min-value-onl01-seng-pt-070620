16f03be121aef7f8fce3bec5245f5723edead9bf
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-17 17:40:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  least_a = nil 
+  least_b = nil 
   name_hash.select do |a,b|
-    if b < b  
-      a 
+    
     end
   end
-  name_hash
 end
